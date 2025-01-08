b07775494be65ba7eb1050a7fe16e145bac93370
Pattuv <161959486+Pattuv@users.noreply.github.com>
2025-01-08 17:57:49 -0500

    fixing issues

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'user' 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password_hash = db.Column(db.String(150), nullable=False)
